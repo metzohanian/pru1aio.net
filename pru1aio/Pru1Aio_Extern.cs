e646c622196d00e7d9ad6d5710cee0daf3a0838b
megiddo <megiddo@fa565761-d09e-4974-89b5-5e209b8e3fef>
2015-02-13 01:08:51 +0000

    initial import

---
+++ pru1aio/Pru1Aio_Extern.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Pru1Aio
+{
+	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+	public delegate void SynchronousCallBack(uint BufferCount, ushort BufferSize, ref Reading[] CapturedBuffer, ref CallState CallState, ref PruSharedMemory PruMemory);
+
+	public partial class Pru1Aio
+	{
+		[DllImport ("libpru1aio.so")]
+		private static extern void pru_rta_configure(ref PruControl Control);
+		[DllImport ("libpru1aio.so")]
+		[return : MarshalAs(UnmanagedType.LPStruct)]
+		private static extern PruControl pru_rta_init();
+
+		[DllImport ("libpru1aio.so")]
+		private static extern void pru_rta_clear_pru(int PRU);
+
+		[DllImport ("libpru1aio.so")]
+		private static extern void pru_rta_start_firmware();
+
+		[DllImport ("libpru1aio.so")]
+		// Incomplete
+		private static extern void pru_rta_start_capture(ref PruSharedMemory PruMemory, SynchronousCallBack CallBack, ref CallState CallState);
+		[DllImport ("libpru1aio.so")]
+		private static extern void pru_rta_pause_capture(ref PruSharedMemory PruMemory);
+		[DllImport ("libpru1aio.so")]
+		private static extern void pru_rta_stop_capture(ref PruSharedMemory PruMemory);
+
+		[DllImport ("libpru1aio.so")]
+		private static extern void print_pru_map(ref PruSharedMemory PruMemory);
+		[DllImport ("libpru1aio.so")]
+		private static extern void print_pru_map_address(ref PruSharedMemory PruMemory);
+
+		[DllImport ("libpru1aio.so")]
+		[return : MarshalAs(UnmanagedType.LPStruct)]
+		private static extern Conditions pru_rta_init_conditions();
+		[DllImport ("libpru1aio.so")]
+		private static extern void pru_rta_destroy_conditions(ref Conditions Conditions);
+		[DllImport ("libpru1aio.so")]
+		private static extern void pru_rta_add_condition(ref Conditions Conditions, string Name, Comparator Condition, Signal Signal, ushort Comp1, ushort Comp2);
+
+		[DllImport ("libpru1aio.so")]
+		private static extern void pru_printf_hello(string world);
+
+
+	}
+}
---
+++ pru1aio/Pru1Aio_Structs.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Pru1Aio
+{
+	[StructLayout (LayoutKind.Sequential, Pack=1)]
+	public unsafe struct PruControl {
+		uint CurrentBuffer;			// 0x0		Complete
+		uint BufferCount;				// 0x4		Complete
+		byte BufferSize;				// 0x8		Complete
+		byte ChannelEnabledMask;		// 0x9		Complete
+		byte SampleAverage;			// 0xA		Complete
+		byte SampleSoc;				// 0xB		Complete
+		uint SampleRate;				// 0xC		Complete
+		byte sample_mode;				// 0x10		Complete
+		byte ChannelCount;				// 0x11		Complete
+		uint BufferMemoryBytes;		// 0X12		Complete
+		uint BufferPosition;			// 0X16		Complete
+		uint ReadCount;				// 0x1A		Complete
+		uint IepClockCount;			// 0x1E
+		uint WriteMask;				// 0x22
+		uint DigitalOut;				// 0x26
+		fixed byte Scratch[14];			// 0x2A
+	}
+
+	public enum Comparator {
+		Greater = 0,
+		GreaterEq,
+		Less,
+		LessEq,
+		Equal,
+		RisingEdge,
+		FallingEdge
+	}
+
+	public enum Signal {
+		CHANNEL_0 = 0,
+		CHANNEL_1,
+		CHANNEL_2,
+		CHANNEL_3,
+		CHANNEL_4,
+		CHANNEL_5,
+		CHANNEL_6,
+		CHANNEL_7,
+		CHANNEL_DIO
+	}
+
+	public enum TriggerState {
+		NOT_TRIGGERED = 0,
+		TRIGGERED
+	}
+
+	[StructLayout (LayoutKind.Sequential, Pack=1)]
+	public unsafe struct PruSharedMemory {
+		PruControl Control;
+		ushort* Buffer1;
+		ushort* Buffer2;
+	}
+
+	public unsafe struct Conditional {
+		fixed char Name[33];
+		Comparator Condition;
+		Signal Signal;
+		ushort Comp1;
+		ushort Comp2;
+
+		int LastSignal;
+
+		TriggerState Triggered;
+		ushort TriggerCount;
+	}
+
+	public unsafe struct Conditions {
+		char Count;
+		Conditional *Conditionals;
+	}
+
+	public unsafe struct Reading {
+		byte Buffer;
+		fixed ushort Readings[8];
+		ushort DigitalIn;
+	}
+
+	public unsafe struct CallState {
+		Reading *Readings;
+		Reading BufferMean;
+		int Records;
+		int MaximumRecords;
+		int Signals;
+		IntPtr CallerState;
+		Conditions *Conditions;
+	}
+
+}
