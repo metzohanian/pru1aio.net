f098a6e68b37025f957ba92906d5925522836d15
megiddo <megiddo@fa565761-d09e-4974-89b5-5e209b8e3fef>
2015-02-13 01:08:51 +0000

    initial import

---
+++ metzohanian/pru1aio.net/pru1aio/Pru1Aio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Pru1Aio
+{
+
+	public partial class Pru1Aio
+	{
+		public Pru1Aio ()
+		{
+		}
+
+		public void ClearPru(int PRU) {
+			Pru1Aio.pru_rta_clear_pru (PRU);
+		}
+
+		public void Hello(string World) {
+			Pru1Aio.pru_printf_hello (World);
+		}
+
+	}
+}
+
---
+++ metzohanian/pru1aio.net/test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace test
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Pru1Aio.Pru1Aio p = new Pru1Aio.Pru1Aio ();
+			p.Hello ("It Worked!");
+		}
+	}
+}
