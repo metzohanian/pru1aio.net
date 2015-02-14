1259aad48638f6b77d1441d5c567ee1fe4d2fb9a
megiddo <megiddo@fa565761-d09e-4974-89b5-5e209b8e3fef>
2015-02-14 16:45:50 +0000

    add support for fill (limit), ring, and overflow modes; add stop() handle for multithreading; still not multithread safe

---
+++ metzohanian/pru1aio.net/pru1aio/Pru1Aio_PublicMembers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Pru1Aio
+{
+
+    public partial class Pru1Aio
+    {
+        public List<int> DroppedBuffers
+        {
+            get
+            {
+                return _DroppedBuffers;
+            }
+        }
+
+        public bool IsCapturing
+        {
+            get
+            {
+                return _IsCapturing;
+            }
+        }
+
+    }
+
+}
